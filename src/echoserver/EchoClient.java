ad19b19c457b87799eef27e9b8f0770178ed95f8
Sphat001 <sphat001@morris.umn.edu>
2017-11-08 14:45:56 -0600

    Still working on EchoClient
    Almost finished

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -5,24 +5,67 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+
+
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
-
+		public static InputStream socketInputStream;
+		public static OutputStream socketOutputStream;
+		public static Socket socket;
 	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
+		
+		socket = new Socket("localhost", PORT_NUMBER);
+		socketInputStream = socket.getInputStream();
+		socketOutputStream = socket.getOutputStream();
+		
+		new userInputThread().run();
+		new userOutputThread().run();
 	}
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
+	
+	public static class userInputThread implements Runnable {
+		
+		
+
+		@Override
+		public void run() {
+			int readByte;
+			try {
+				while((readByte = System.in.read()) != -1) {
+					socketOutputStream.write(readByte);
+				}
+			
+				socketOutputStream.flush();
+				socket.shutdownOutput();
+			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
-		System.out.flush();
+		
+	}
+	
+	public static class userOutputThread implements Runnable {
+
+		@Override
+		public void run() {
+			int readByte;
+			try {
+				while((readByte = socketInputStream.read()) != -1) {
+					System.out.write(readByte);
+				}
+			
+				System.out.flush();
+				socket.close();
+			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 }
